914edf8b53bda7c2c4c2220d7d6b45286337bcb4
Petrus Repo <petrus.repo@iki.fi>
2009-05-21 15:14:40 +0000

    CLIENT_NAME

--- setup_files/koala_authentication_config.rb
+++ setup_files/koala_authentication_config.rb
@@ -1,11 +1,14 @@
 module KoalaAuthenticationConfig
 
+  # Current project's client name.
+  # N.B. This must match the according name from the server.
+  CLIENT_NAME = "kihla"
+  
   # Private key is not known for authentication clients.
   CLIENT_CONFIGURATION = true
 
   # If CLIENT_CONFIGURATION == true, then you don't need the private key.
   SERVER_PRIVATE_KEY_FILE = File.join("#{RAILS_ROOT}", "/config", 'private_server_key.pem')
   SERVER_PUBLIC_KEY_FILE  = File.join("#{RAILS_ROOT}", "/config", 'public_server_key.pem')
-
-  
+ 
 end
