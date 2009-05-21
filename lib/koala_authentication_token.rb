529899617dfa7542addd6916b010ca3bccce0a53
Petrus Repo <petrus.repo@iki.fi>
2009-05-21 11:04:36 +0000

    decryption

--- lib/koala_authentication_token.rb
+++ lib/koala_authentication_token.rb
@@ -8,40 +8,47 @@ class KoalaAuthenticationToken
   @@private_key = OpenSSL::PKey::RSA.new(File.read(KoalaAuthenticationConfig::SERVER_PRIVATE_KEY_FILE)) unless KoalaAuthenticationConfig::CLIENT_CONFIGURATION
   @@public_key = OpenSSL::PKey::RSA.new(File.read(KoalaAuthenticationConfig::SERVER_PUBLIC_KEY_FILE))  
     
+  # Parameter data is defined as follows:
+  #  when :plain ==> Plain-text Login-XML data
+  #  when :encrypted ==> Data is a following hash:
+  #      data = { :key => "base64-encoded encrypted AES-key",
+  #               :iv  => "base64-encoded encrypted AES initialization vector",
+  #               :data => "base64-encoded encrypted login-XML data" }
   def initialize(data, format)
     case format when :plain
-      self.encrypt_sensitive(plain_data)
+      self.plain_data = data
+      self.encrypt_sensitive
     when :encrypted
-      self.decrypt_sensitive(data)
+      self.base64_data = data[:data]
+      self.base64_key  = data[:key]
+      self.base64_iv   = data[:iv]
+      self.decrypt_sensitive
     else
       raise ArgumentError.new("Incorrect format parameter '#{format}', only :plain and :encrypted allowed.")
     end
   end
   
+  private
+  
   def decrypt_sensitive(data = {})  
-    raise "data: #{data.inspect}"
     cipher = OpenSSL::Cipher::Cipher.new('aes-256-cbc')  
     cipher.decrypt  
-    cipher.key = @@private_key.private_decrypt(self.encrypted_key)  
-    cipher.iv = @@private_key.private_decrypt(self.encrypted_iv)  
+    cipher.key = @@private_key.private_decrypt(Base64.decode64(self.base64_key))  
+    cipher.iv = @@private_key.private_decrypt(Base64.decode64(self.base64_iv))  
 
-    decrypted_data = cipher.update(self.encrypted_data)  
-    decrypted_data << cipher.final  
-  end  
-  # 
-  #def clear_sensitive  
-  #  self.encrypted_data = self.encrypted_key = self.encrypted_iv = nil  
-  #end  
+    decrypted_data = cipher.update(Base64.decode64(self.base64_data)
+    decrypted_data << cipher.final
+    
+    self.plain_data = decrypted_data
+  end    
   
-  protected
-  
-  def encrypt_sensitive(plain_data)
+  def encrypt_sensitive
     cipher = OpenSSL::Cipher::Cipher.new('aes-256-cbc')  
     cipher.encrypt  
     cipher.key = random_key = cipher.random_key  
     cipher.iv = random_iv = cipher.random_iv  
 
-    encrypted_data = cipher.update(plain_data)  
+    encrypted_data = cipher.update(self.plain_data)  
     encrypted_data << cipher.final 
     self.base64_data = Base64.encode64(encrypted_data) 
 
