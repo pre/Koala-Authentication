35dc78270d1ec7d0f2cec044aeaa230b6a1d72f2
Petrus Repo <petrus.repo@iki.fi>
2010-07-15 21:02:23 +0300

    updated default config

--- setup_files/koala_authentication_config.rb
+++ setup_files/koala_authentication_config.rb
@@ -1,20 +1,25 @@
 module KoalaAuthenticationConfig
 
-  # Current project's client name.
-  # N.B. This must match the according name from the server.
-  CLIENT_NAME = "unset"
+  # Current project's client name delivered in the LoginXml
+  CLIENT_NAME = Lintuvaara::ApiConfig::SERVER_ACCOUNT
   
-  # Private key is not known by the authentication clients.
+  # Private key is not known for authentication clients.
   CLIENT_CONFIGURATION = true
 
+  SERVER_HOST = Lintuvaara::ApiConfig::SERVER_HOST
+  SERVER_PROTOCOL = Lintuvaara::ApiConfig::SERVER_PROTOCOL
+  
   # URL to the authentication server (do not include trailing slash)
-  SERVER_URL = "http://localhost:3000"
+  SERVER_URL = "#{SERVER_PROTOCOL}#{SERVER_HOST}"
   
   # URL to the authentication server's logout url
   SERVER_LOGOUT_URL = SERVER_URL + "/sessions/logout"
   
-  # If CLIENT_CONFIGURATION == true, then you don't need the private key.
-  SERVER_PRIVATE_KEY_FILE = File.join("#{RAILS_ROOT}", "/config", 'private_server_key.pem')
-  SERVER_PUBLIC_KEY_FILE  = File.join("#{RAILS_ROOT}", "/config", 'public_server_key.pem')
-
+  # URL to user preferences
+  SERVER_USER_PREFERENCES_URL = SERVER_URL + "/user_preferences"
+  
+  # If CLIENT_CONFIGURATION then you don't need the private key.
+  SERVER_PRIVATE_KEY_FILE = ""
+  SERVER_PUBLIC_KEY_FILE  = File.join("#{RAILS_ROOT}", "/config", 'lintuvaara_public_key.pem')
+  
 end
